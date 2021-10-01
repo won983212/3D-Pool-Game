d9a005bf7a79401c499f48bfe30a3688447ba699
이현 <won983212@naver.com>
2021-10-01 13:15:54 +0900

    Added ball collision resolving

--- CGTermProject/src/pooltable.cpp
+++ CGTermProject/src/pooltable.cpp
@@ -6,11 +6,11 @@
 PoolTable::PoolTable(glm::vec2 size)
 	: size(size)
 {
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 20; i++)
 	{
 		Ball* ball = addBall(randomVal(-size.x / 2, size.x / 2), randomVal(-size.y / 2, size.y / 2));
-		ball->velocity.x = randomVal(-10, 10);
-		ball->velocity.y = randomVal(-10, 10);
+		ball->velocity.x = randomVal(-30, 30);
+		ball->velocity.y = randomVal(-30, 30);
 	}
 }
 
@@ -25,15 +25,46 @@ void PoolTable::update(float partialTime) const
 	for (unsigned int i = 0; i < balls.size(); i++)
 	{
 		Ball* ball = balls[i];
-		glm::vec2 vel = ball->velocity;
+
+		// update velocity
+		float vel = glm::length(ball->velocity);
+		if (vel > 0)
+			ball->velocity *= 0.99; // friction
 
 		// update position
-		ball->position += partialTime * vel;
+		ball->position += partialTime * ball->velocity;
 
 		// update rotation
-		glm::vec3 rotationAxis = glm::cross(glm::vec3(0, 1, 0), glm::vec3(vel.x, 0, vel.y));
-		float dAngle = partialTime * glm::length(vel) / BALL_RADIUS;
-		ball->rotation = glm::angleAxis(dAngle, glm::normalize(rotationAxis)) * ball->rotation;
+		if (vel > 0)
+		{
+			glm::vec3 vel3d = glm::vec3(ball->velocity.x, 0, ball->velocity.y);
+			glm::vec3 rotationAxis = glm::cross(glm::vec3(0, 1, 0), vel3d);
+			float dAngle = partialTime * glm::length(ball->velocity) / BALL_RADIUS; // dt*|v|/r
+			ball->rotation = glm::angleAxis(dAngle, glm::normalize(rotationAxis)) * ball->rotation;
+		}
+
+		// collision check & resolving. 1회만 test하기 위해 범위는 0~i로 설정.
+		for (unsigned int j = 0; j < i; j++)
+		{
+			float dx = balls[i]->position.x - balls[j]->position.x;
+			float dy = balls[i]->position.y - balls[j]->position.y;
+
+			// collision test success
+			float penetration_sq = 4 * BALL_RADIUS * BALL_RADIUS - (dx * dx + dy * dy);
+			if (penetration_sq > 0)
+			{
+				// apply impulse
+				glm::vec2 jNorm = glm::normalize(balls[j]->position - balls[i]->position);
+				glm::vec2 impulse = glm::dot(balls[j]->velocity - balls[i]->velocity, jNorm) * jNorm;
+				balls[i]->velocity += impulse;
+				balls[j]->velocity -= impulse;
+
+				// position correction
+				float penetration = 2 * BALL_RADIUS - sqrt(dx * dx + dy * dy);
+				balls[i]->position += penetration / 2 * -jNorm;
+				balls[j]->position += penetration / 2 * jNorm;
+			}
+		}
 
 		if (ball->position.x < -size.x / 2 + BALL_RADIUS)
 		{
